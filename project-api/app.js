bdabdd318743eb5ee4af0662d3f1b3cdc673de04
leonardo-xompero <leonardo.xompero@studenti.unitn.it>
2020-11-24 15:12:11 +0100

    Changed path to 'uploads' and changed static in app.js

--- project-api/app.js
+++ project-api/app.js
@@ -28,7 +28,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/',express.static('public'));
+app.use('/',express.static('project-api/public'));
 
 //CORS
 app.use((req,res,next)=>{
