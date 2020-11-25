ea7cd4c8bd90a48bd658f86fa29ba9230e6fee71
leonardo-xompero <leonardo.xompero@studenti.unitn.it>
2020-11-25 15:29:50 +0100

    New deploy

--- project-api/app.js
+++ project-api/app.js
@@ -28,7 +28,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', express.static('../project-web'));
+app.use('/', express.static('project-web'));
 
 //CORS
 app.use((req, res, next) => {
