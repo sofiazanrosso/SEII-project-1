c76bd21cfaa7f14fdb1a384ddf178218f79ed147
MartinaVin <martina.vincoli@studenti.unitn.it>
2020-11-30 16:16:52 +0100

    Fixed categories buttons on the homepage

--- project-api/api/models/category.js
+++ project-api/api/models/category.js
@@ -1,14 +1,10 @@
 const mongoose= require('mongoose');
 
-// const Announcement= require('../models/announcement');
-// const Flyer = require('../models/flyer');
-
 // general schema for a category
 const categorySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: {type: String, required:true}
-    // ann_ids: {type: Array, required: true},
-    // fly_ids: {type: Array, required: true}
+
 });
 
 module.exports = mongoose.model('Category',categorySchema);
